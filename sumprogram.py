8361d287a57536a7a264acfdaeb2ac949fed08fa
zeenat65415 <108066409+zeenat65415@users.noreply.github.com>
2022-07-08 03:04:18 -0700

    string

--- sumprogram.py
+++
@@ -1,8 +0,0 @@
-# python ptogram to add two numbers
-number1 = input("first number:")
-number2 = input("\n second number:")
-sum = float(number1) + float (number2)
-print ("the sum of {0} and {1} is {2}" .format(number1,number2,sum))
-
-
-
