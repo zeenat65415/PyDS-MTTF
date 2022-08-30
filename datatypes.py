12d191d06eec5a4c2a51964546b69bbb9ef0fee8
zeenat65415 <108066409+zeenat65415@users.noreply.github.com>
2022-08-30 07:31:51 -0700

    Create datatypes.py

---
+++ datatypes.py
@@ -0,0 +1,46 @@
+x=1.00
+y=-2345
+z=765432
+a=1.10
+b=1.0
+c=-35.59
+d=87.7e100
+print(x,type(x))
+print(y,type(y))
+print(z,type(z))
+print(a,type(a))
+print(b,type(b))
+print(c,type(c))
+print(d,type(d))
+name="bruce wayne 01"
+color='yellow'
+Institute="Digipodium"
+print(name,type(name))
+print(color,type(color))
+print(Institute,type(Institute))
+is_coding=True
+is_fun=True
+are_u_bored=False
+print(is_coding,type(is_coding))
+print(is_fun,type(is_fun))
+print(are_u_bored,type(are_u_bored))
+e=None
+print(e,type(e))
+l=[5,10,15,20,25,30,35,40]
+vals=[1,2.2,'python']
+colors=['red','green','yellow']
+print(l,type(l))
+print(vals,type(vals))
+print(colors,type(colors))
+t={5,10,15,20,25,30,35,40}
+vals1={1,2.2,'python'}
+colors1={'red','green','yellow'}
+print(t,type(t))
+print(vals1,type(vals1))
+print(colors1,type(colors1))
+car_info={
+    "brand":"Ford",
+    "model1":"Mustang",
+    "year":1964
+}
+print(car_info,type(car_info))
