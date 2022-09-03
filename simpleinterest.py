e1c2355e147c0aa56f5e86fedae92ee8d219acec
zeenat65415 <108066409+zeenat65415@users.noreply.github.com>
2022-09-03 00:59:07 -0700

    Update simpleinterest.py

--- simpleinterest.py
+++ simpleinterest.py
@@ -4,5 +4,5 @@ rate=float(input("Enter rate=>"))
 time=float(input("Enter time=>"))
 SI=principle*rate*time/100
 amount=principle+SI
-print(F"Simple Interest is:{SI}")
+print(f"Simple Interest is:{SI}")
 print(f"Amount is:{amount}")
