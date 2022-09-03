6c042723117df87b5216c8ab5ee8b4aec824bf84
zeenat65415 <108066409+zeenat65415@users.noreply.github.com>
2022-09-03 00:41:50 -0700

    Update area.py

--- area.py
+++ area.py
@@ -8,11 +8,11 @@ print("Area of rectangle is:",area)
 
 #area of square
 side=float(input("Enter side of a square=>"))
-area1=side**2
-print("Area of square is:",area1)
-print(f"Area of square is:{area1}")
+area=side**2
+print("Area of square is:",area)#comma separation
+print(f"Area of square is:{area}")#f-string
 
 #area of circle
 radius=float(input("Enter radius of a circle=>"))
-area2=3.14*radius**2
-print(f"Area of circle is:{area2}" )
+area=3.14*radius**2
+print(f"Area of circle is:{area}" )
