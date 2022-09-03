4cfcde5f257a2585207b64b01834aff2e5d68736
zeenat65415 <108066409+zeenat65415@users.noreply.github.com>
2022-09-03 00:38:52 -0700

    Create area.py

---
+++ area.py
@@ -0,0 +1,18 @@
+#area oof rectangle
+length=input("Enter length of a rectangle=>")
+breadth=input("Enter breadth of a rectangle=>")
+length=int(length)
+breadth=int(breadth)
+area=length*breadth
+print("Area of rectangle is:",area)
+
+#area of square
+side=float(input("Enter side of a square=>"))
+area1=side**2
+print("Area of square is:",area1)
+print(f"Area of square is:{area1}")
+
+#area of circle
+radius=float(input("Enter radius of a circle=>"))
+area2=3.14*radius**2
+print(f"Area of circle is:{area2}" )
