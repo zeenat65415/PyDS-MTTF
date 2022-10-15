d6a7263ab8feb19db42714d9a990db4c2acf375d
zeenat65415 <108066409+zeenat65415@users.noreply.github.com>
2022-10-14 22:40:26 -0700

    Datamining

---
+++ data_mining/flipkart_bot.py
@@ -0,0 +1,75 @@
+import os
+import requests
+import datetime
+import pandas as pd
+from bs4 import BeautifulSoup
+
+def get_soup(url):
+    r = requests.get(url)
+    if r.status_code == 200:
+        return BeautifulSoup(r.text,"html5lib")
+    return None    
+    _
+def get_products(soup):
+    target = soup.find("div" , class_ ="_1YokD2 _3Mn1Gg")
+    if target is not None:
+      items = target.find_all("div", class_ = "_1AtVbE col-12-12")
+      print(f"Found {len(items)} items")
+      return items
+
+def extract_one(item):
+    data = {}
+    title = item.find("div",class_="_4rR01T")
+    sell_price=item.find("div",class_="_30jeq3 _1_WHN1")
+    orig_price=item.find("div",class_="_3I9_wc _27UcVY")
+    rating=item.find("div",class_="_3LWZlK")
+    num_ratings_reviews=item.find("span",class_="_2_R_DZ")
+    if title :
+        data["title"]= title.text.strip()
+    if sell_price :
+        data["sell_price"] = sell_price.text.strip()
+    if orig_price:
+        data["original_price"] = orig_price.text.strip()
+    if rating:
+        data["rating"] = rating.text.strip()
+    if num_ratings_reviews:
+        data["num_ratings_reviews"] = num_ratings_reviews.text.strip() 
+    return data    
+
+def main(query="tv"):
+    base_url="https://www.flipkart.com/search" 
+    pos = 1
+    all_products = []
+    while True:
+        url = f"{base_url}?q={query}&page={pos}"
+        print(url)
+        soup = get_soup(url)
+        if soup is None:
+            print("No data found")
+            break
+        items = get_products(soup)
+        if len(items) == 0:
+            break
+        for item in items:
+            data = extract_one(item)
+            all_products.append(data) 
+        pos += 1
+    if len(all_products) > 0:
+        time = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+        pd.DataFrame(all_products).to_csv(f"flipkart_{query}_{time}.csv", index = False)
+
+if __name__== "_main_":
+    main("mobiles")          
+
+
+         
+
+
+
+
+
+
+  
+
+
+
---
+++ data_mining/latest_news_bot.py
@@ -0,0 +1,56 @@
+import os
+import requests
+import datetime
+import pandas as pd
+from bs4 import BeautifulSoup
+
+def get_soup(url):
+    r = requests.get(url)
+    if r.status_code == 200:
+        return BeautifulSoup(r.text,"html5lib")
+    return None    
+
+def get_news(item):
+    data = {}
+    heading = item.find("h2",class_="newsHdng")
+    source=item.find("span",class_="posted-by")
+    content=item.find("p",class_="newsCont")
+    if heading is not None:
+        data["heading"] = heading.text.strip()
+    if source is not None:
+        data["source"]  = source.text.strip()
+    if content is not None:
+        data["content"] = content.text.strip()
+    return data    
+
+def get_news_list(soup):
+    headlines = []
+    news = soup.find("div",class_="lisingNews")
+    for item in news.find_all("div",class_="news_Itm"):
+        headlines.append(get_news(item))
+    return headlines
+
+def main(filename="latest_news.csv"):
+    pos = 1
+    base_url = "https://www.ndtv.com/latest" 
+    all_headlines = []
+    while True:
+        url =f"{base_url}/page-{pos}"
+        print("➡",url)
+        soup = get_soup(url)
+        if soup is None:
+            break
+        else:
+            headlines = get_news_list(soup)
+            if headlines:
+                all_headlines.extend(headlines)
+            pos += 1
+    #save
+    time = datetime.datetime.now().strftime("%Y_%m_%d_%H")
+    filename=f"{time}_{filename}"
+    pd.DataFrame(all_headlines).to_csv(filename, index = False)        
+        
+
+
+if __name__ == "_main_" :
+    main()               
