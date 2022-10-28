7d1c55b9402fcffccd02f9a093b9308a70832d8e
zeenat65415 <108066409+zeenat65415@users.noreply.github.com>
2022-10-28 00:31:52 -0700

    functions

--- anonymous_func.py
+++ anonymous_func.py
@@ -1,11 +1,11 @@
-nums=[1,2,3,5,6,7,8,9,10]
+nums=[1,2,3,4,1,2,3,5,6,7,3,3,2,1,2,3,5,6]
 names=['Ajay Singh','Vijay Pratap','Gunja Thakur' ]
 
 num_sqr = list(map(lambda i:i** 2,nums))
 print(num_sqr)
 
-num_eql=list(map(lambda i: i+4* i**2,nums))
+num_eq1=list(map(lambda i: i+4* i**2,nums))
 print(num_eq1)
 
-first_names=tuple(map(lambda nm: nm.split([0],names)))
+first_names=tuple(map(lambda nm: nm.split()[0],names))
 print(first_names)
