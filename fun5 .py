c96a74eee6af9a85df269c4e04e2a32bd90b79e8
zeenat65415 <108066409+zeenat65415@users.noreply.github.com>
2022-10-28 00:01:01 -0700

    Update fun5 .py

--- fun5 .py
+++ fun5 .py
@@ -8,12 +8,14 @@ def mean(*numbers):
 
 mean(1,2,3,4)
 
+
 def mean(*numbers):
   if not numbers:  
-    return None
+      return None
   return sum(numbers)/len(numbers)
 
 print(mean(1,2,3,4))
 print(mean(1,2,3,4,5,6))
 print(mean(1,22,1,1,2,3,1,321,123,1,312))
 print(mean())
+print(mean(10))
